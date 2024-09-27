397f97e9b25540ce6f81c2f53e39ab3a7252940f
= <rantsajonathan@gmail.com>
2024-09-27 11:21:20 +0300

    presque ok le design

--- src/screen/Team/Team.js
+++ src/screen/Team/Team.js
@@ -1,32 +1,63 @@
 import React, { useState, useEffect } from "react";
 import "./Team.css";
+import image1 from "../../assets/Mira.jpeg";
+import image2 from "../../assets/Valicio.jpeg";
+import image3 from "../../assets/Rantsa.jpeg";
+import image4 from "../../assets/Larry.jpeg";
+import image5 from "../../assets/Rominah.jpeg";
+import image6 from "../../assets/Ranja.jpeg";
+import image7 from "../../assets/Hony.jpeg";
 
 const Team = () => {
   const [cards] = useState([
-    { image: "Image1", name: "Nom1", description: "Description1" },
-    { image: "Image2", name: "Nom2", description: "Description2" },
-    { image: "Image3", name: "Nom3", description: "Description3" },
-    { image: "Image4", name: "Nom4", description: "Description4" },
-    { image: "Image5", name: "Nom5", description: "Description5" },
-    { image: "Image6", name: "Nom6", description: "Description6" },
-    { image: "Image7", name: "Nom7", description: "Description7" },
+    {
+      image: image1,
+      name: "Mira",
+      description:
+        "En tant que porteur du projet SPARE et développeur hardware, je supervise la vision globale tout en concevant les modules connectés. Mon rôle est de diriger l’équipe vers nos objectifs tout en assurant la performance technique des solutions que nous proposons.",
+    },
+    {
+      image: image2,
+      name: "Valicio",
+      description:
+        "Je suis le résponsable du système embarqué de SPARE, chargé de concevoir les modules connectés qui mesurent et contrôlent la consommation d'énergie et d'eau, assurant des données précises en temps réel.",
+    },
+    {
+      image: image3,
+      name: "Rantsa",
+      description:
+        "Chargé de créer une application intuitive qui permet de suivre et gérer en temps réel la consommation d'énergie et d'eau. Mon objectif est d'offrir une expérience fluide et efficace aux utilisateurs.",
+    },
+    {
+      image: image4,
+      name: "Larry",
+      description:
+        "En tant que développeur back-end chez SPARE, je suis responsable de la gestion des serveurs et de l’architecture qui supportent l’application et les modules connectés. Mon rôle est de garantir des échanges de données sécurisés et performants entre les utilisateurs et notre système.",
+    },
+    { image: image5, name: "Rominah", description: "Description2" },
+    {
+      image: image6,
+      name: "Ranja",
+      description:
+        "Je supervise la gestion des finances et développe des stratégies budgétaires pour assurer la stabilité et la croissance à long terme du projet. Mon rôle inclut  la gestion des ressources financières et la recherche de fonds pour soutenir notre expansion tout en garantissant une gestion saine et durable.",
+    },
+    { image: image7, name: "Hony", description: "Description3" },
   ]);
 
   const [visibleCards, setVisibleCards] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [cardsPerPage, setCardsPerPage] = useState(3); // Default to 3
+  const [cardsPerPage, setCardsPerPage] = useState(3);
 
-  // Update cards per page based on window size
   useEffect(() => {
     const updateCardsPerPage = () => {
       if (window.innerWidth <= 500) {
-        setCardsPerPage(2); // Show 2 cards on smaller screens
+        setCardsPerPage(2);
       } else {
-        setCardsPerPage(3); // Default is 3 cards
+        setCardsPerPage(3);
       }
     };
 
-    updateCardsPerPage(); // Initial check
+    updateCardsPerPage();
     window.addEventListener("resize", updateCardsPerPage);
 
     return () => {
@@ -60,9 +91,9 @@ const Team = () => {
         <div className="cards-container">
           {visibleCards.map((card, index) => (
             <div className="Card" key={index}>
-              <h2>{card.image}</h2>
+              <img src={card.image} alt={card.name} className="team-image" />
               <h2>{card.name}</h2>
-              <h2>{card.description}</h2>
+              <h4>{card.description}</h4>
             </div>
           ))}
         </div>
